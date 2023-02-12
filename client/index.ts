dd4bffeaa82f0848b77bc99946b8d1e9e062f30b
johnsoncodehk <johnsoncodehk@gmail.com>
2023-02-12 10:53:21 +0800

    refactor: move Component, Layout outside `setup()`

--- client/index.ts
+++ client/index.ts
@@ -6,29 +6,28 @@ export default function (app: App) {
 
 	if (location && location.pathname.startsWith('/__preview/')) {
 
+		const importPath = location.pathname.substring('/__preview'.length);
+		const Component = defineAsyncComponent(() => import(/* @vite-ignore */importPath));
+		const Layout = defineAsyncComponent(() => import(/* @vite-ignore */importPath + '__preview.vue'));
+
+		if (import.meta.hot) {
+			fireHash();
+			window.addEventListener('hashchange', fireHash);
+		}
+
 		(app._component as DefineComponent).setup = () => {
-
-			const importPath = location.pathname.substring('/__preview'.length);
-			const Component = defineAsyncComponent(() => import(/* @vite-ignore */importPath));
-			const Layout = defineAsyncComponent(() => import(/* @vite-ignore */importPath + '__preview.vue'));
-
-			if (import.meta.hot) {
-				fireHash();
-				window.addEventListener('hashchange', fireHash);
-			}
-
 			return () => h(Layout, undefined, {
 				default: (props: any) => h(Component, props)
 			});
-
-			function fireHash() {
-				try {
-					import.meta.hot?.send('vue-component-preview:hash', {
-						file: importPath,
-						text: location.hash ? atob(location.hash.substring(1)) : '',
-					});
-				} catch { }
-			}
 		};
+
+		function fireHash() {
+			try {
+				import.meta.hot?.send('vue-component-preview:hash', {
+					file: importPath,
+					text: location.hash ? atob(location.hash.substring(1)) : '',
+				});
+			} catch { }
+		}
 	}
 }
